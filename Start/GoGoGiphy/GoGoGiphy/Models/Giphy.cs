fa69f7db597d9566835941c6c251ff8aa72db4dc
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-22 14:26:02 -0700

    Update Giphy.cs

--- Start/GoGoGiphy/GoGoGiphy/Models/Giphy.cs
+++ Start/GoGoGiphy/GoGoGiphy/Models/Giphy.cs
@@ -9,12 +9,15 @@ namespace GoGoGiphy.Core.Models
     {
 
         [JsonProperty("data")]
+        
         public IList<Datum> Data { get; set; }
 
         [JsonProperty("pagination")]
+        
         public Pagination Pagination { get; set; }
 
         [JsonProperty("meta")]
+        
         public Meta Meta { get; set; }
     }
 }
